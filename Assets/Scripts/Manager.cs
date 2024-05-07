10afc812267f455378da5b5cd3aeea1e79bdd04a
Sıray Tarım <siiraytaarim@hotmail.com>
2024-05-07 09:39:43 +0300

    Deneme

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool isGameStarted;
+
+    public GameObject gameOverPanel;
+    public GameObject startingText;
+
+    public static int numberOfCoins;
+
+    public Text coinsText;
+    void Start()
+    {
+        gameOver = false;
+        Time.timeScale = 1;
+        numberOfCoins = 0;
+        isGameStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+        }
+        coinsText.text = "Coins: " + numberOfCoins;
+
+        if (Input.GetMouseButton(0))   // 0 for mouse left button
+        {
+            isGameStarted = true;
+            Destroy(startingText);
+        }
+
+    }
+}
